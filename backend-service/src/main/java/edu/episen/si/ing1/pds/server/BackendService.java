20906afc17f7acb1876c59c91768b5138b409764
Sarra <sarra.amara@icloud.com>
2021-01-29 15:38:04 +0100

    Add script and updated BackendService Class

--- backend-service/src/main/java/edu/episen/si/ing1/pds/server/BackendService.java
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/server/BackendService.java
@@ -1,5 +1,27 @@
 package edu.episen.si.ing1.pds.server;
 
-public class BackendService {
+import java.text.ParseException;
+import java.util.Scanner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BackendService {
+private final static Logger logger=LoggerFactory.getLogger(BackendService.class.getName());
+	public static void main(String[] args) throws ParseException {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Veuillez saisir l'option: ");
+		String option= sc.nextLine();
+		boolean inTestMode=false;
+		int maxConnectionValue=10;
+		  if(option.equals("testMode")) {
+			inTestMode=true;
+		}
+		else if(option.equals("maxConnection")) {
+			inTestMode=true;
+			maxConnectionValue = sc.nextInt();
+		}
+		logger.info("Backend service is running. (testMode={"+inTestMode+"}), maxConnection={"+ maxConnectionValue +"}");
+		
+	}
 }
---
+++ backend-service/src/main/scripts/backend-service.bat
@@ -0,0 +1,1 @@
+javac C:\Users\user16\new-city\backend-service\src\main\java\edu\episen\si\ing1\pds\server\BackendService.java
