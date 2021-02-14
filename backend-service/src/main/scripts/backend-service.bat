fbebb1375be1a982e3a517fbe269cbefb0acc5b4
Sarra <sarra.amara@icloud.com>
2021-02-14 17:39:26 +0100

    Updated backend-service.bat

--- backend-service/src/main/scripts/backend-service.bat
+++ backend-service/src/main/scripts/backend-service.bat
@@ -1,7 +1,4 @@
 
-set CLASSPATH=C:\Users\user16\new-city\backend-service\target\classes;C:\Users\user16\.m2\repository\ch\qos\logback\logback-classic\1.1.7\logback-classic-1.1.7.jar;C:\Users\user16\.m2\repository\ch\qos\logback\logback-core\1.1.7\logback-core-1.1.7.jar;C:\Users\user16\.m2\repository\org\slf4j\slf4j-api\1.7.20\slf4j-api-1.7.20.jar;C:\Users\user16\.m2\repository\commons-cli\commons-cli\1.4\commons-cli-1.4.jar;
-set arg1=%1
-set arg2=%2
-set arg3=%3
-set arg4=%4
-java edu.episen.si.ing1.pds.server.BackendService %arg1% %arg2% %arg3% %arg4%
+set m2=%M2_REPO%
+set clsspth=%m2%/edu/episen/si/ing1/pds/backend-service/0.0.1-SNAPSHOT/backend-service-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+call java -cp %clsspth% edu.episen.si.ing1.pds.server.BackendService %*
