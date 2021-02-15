e6db1cacae956314c9e326ed779e364acf2866ab
hejerfessi <hejer.fessi@etu.u-pec.fr>
2021-02-15 22:57:39 +0100

    Delete backend-service.sh

--- backend-service/src/main/scripts/backend-service.sh
+++
@@ -1,3 +0,0 @@
-classpat="${M2_REPO}/edu/episen/si/ing1/pds/backend-service/0.0.1-SNAPSHOT"
-jarfile="${classpat}/backend-service-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
-exec java -cp ${jarfile} edu.episen.si.ing1.pds.server.BackendService ${*}
