ea5b4d3c81977d778884dc15b23c65e87be39b0d
hejerfessi <hejer.fessi@etu.u-pec.fr>
2021-02-15 22:52:57 +0100

    Delete client.sh

--- client/src/main/scripts/client.sh
+++
@@ -1,3 +0,0 @@
-classpat="${M2_REPO}/edu/episen/si/ing1/pds/client/0.0.1-SNAPSHOT"
-jarfile="${classpat}/client-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
-exec java -cp ${jarfile} edu.episen.si.ing1.pds.client.Client ${*}
