598914f73bd9e7b0c7b311341f0b1342168e4c17
Sarra <sarra.amara@icloud.com>
2021-02-19 14:29:00 +0100

    Update JDBCConnectionPool

--- backend-service/src/main/java/connectionPool/JDBCConnectionPool.java
+++ backend-service/src/main/java/connectionPool/JDBCConnectionPool.java
@@ -1,24 +1,27 @@
 package connectionPool;
+import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Properties;
 public class JDBCConnectionPool {
  private ArrayList<Connection> pool=new ArrayList<Connection>();
- 
- String url="";
- 
- JDBCConnectionPool(){ //Constructor
-	 try {
-		 Class.forName("org.postgresql.Driver");
-		 
-	 }
-	 catch(Exception e) {}
- }
+ protected Properties props;
+
+ public Properties getProps() {
+		return this.props;
+	}
  
   public void createConnectionsPool(int numberOfConnections) {
 	 try {
+		 props=new Properties();
+		 InputStream inStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("app.properties");
+		 props.load(inStream);
+		 Class.forName(props.getProperty("database.driverClassName"));
+		 
 		 for(int i=0;i<numberOfConnections;i++) {
-		 Connection cnx=DriverManager.getConnection(url,"user","password");
-		 pool.add(cnx);
+			 Connection cnx =null;
+			 cnx=DriverManager.getConnection(props.getProperty("database.url"), props.getProperty("database.userName"), props.getProperty("database.password"));
+		     pool.add(cnx);
 
 	   }
 	 }
