6cdad1a97c0b8d5b889a1b131750e2db68aadc79
Sarra <sarra.amara@icloud.com>
2021-02-28 12:45:09 +0100

    Add testConnectVM

---
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/server/TestConnectVM.java
@@ -0,0 +1,19 @@
+package edu.episen.si.ing1.pds.server;
+import java.sql.Connection;
+import connectionPool.*;
+public class TestConnectVM {
+
+	public static void main(String[] args) {
+		try {
+			DataSource.loadPool(5);
+			Connection cnx=DataSource.getConnectionFromPool();
+			if(cnx!=null) {
+				System.out.println("Connected to the database");
+			}
+			DataSource.closePool();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
