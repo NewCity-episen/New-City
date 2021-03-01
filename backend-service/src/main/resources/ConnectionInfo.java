ee2ba6f115cb41fae09f36b5922958ac4efbef80
MohamedTloh <tlohmohamed@hotmail.fr>
2021-03-01 12:14:02 +0100

    structure classe Connection info

---
+++ backend-service/src/main/resources/ConnectionInfo.java
@@ -0,0 +1,19 @@
+public class ConnectionInfo {
+	
+	private String driverClassName;
+	private String url;
+	private String username;
+	private String password;
+	
+	public ConenctionInfo() {
+		
+	}
+	
+	/*
+	database.driverClassName= org.postgresql.Driver
+	database.url= jdbc:postgresql://172.31.249.76/test
+	database.username =postgres
+	database.password =root
+	*/
+
+}
