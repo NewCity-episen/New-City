d419e9e2527f80b8350e8029958c9d31001f3f55
MohamedTloh <tlohmohamed@hotmail.fr>
2021-03-03 19:31:42 +0100

    JDBCConnectionPool now use InfoConnection to get info from properties file

--- backend-service/src/main/java/connectionPool/JDBCConnectionPool.java
+++ backend-service/src/main/java/connectionPool/JDBCConnectionPool.java
@@ -8,65 +8,59 @@ import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 public class JDBCConnectionPool {
- private ArrayList<Connection> pool=new ArrayList<Connection>();
- protected Properties props;
- private final static Logger logger=LoggerFactory.getLogger(JDBCConnectionPool.class.getName());
- public Properties getProps() {
-		return props;
+	
+	private final static Logger logger=LoggerFactory.getLogger(JDBCConnectionPool.class.getName());
+	private ArrayList<Connection> pool=new ArrayList<Connection>();
+
+ 	public void createConnectionsPool(int numberOfConnections) {
+ 		try {
+	 		InfoConnection info = InfoConnection.getINFO();
+	 		
+			for(int i=0;i<numberOfConnections;i++) {
+				Connection cnx =null;
+				cnx = DriverManager.getConnection(info.getUrl()), info.getUsername()), info.getPasswor());
+				pool.add(cnx);
+			}
+		}
+ 		catch(SQLException | IOException | ClassNotFoundException e) {
+			logger.info("Failed to get a connection from database.");
+			e.printStackTrace();
+ 		}
+ 	}
+ 
+	public Connection getConnectionPool() {
+		if(!pool.isEmpty()) {
+			Connection cnx= pool.get(0);
+			pool.remove(0);
+			return cnx;
+		}
+		return null;
 	}
  
-  public void createConnectionsPool(int numberOfConnections) {
-	 try {
-		 
-		 props=new Properties();
-		 InputStream inStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("app.properties");
-		 props.load(inStream);
-		 Class.forName(props.getProperty("database.driverClassName"));
-	
-		 for(int i=0;i<numberOfConnections;i++) {
-			 Connection cnx =null;
-			 cnx=DriverManager.getConnection(props.getProperty("database.url"),
-					 props.getProperty("database.username"), props.getProperty("database.password"));
-			 pool.add(cnx);
-	   }
-	 }
-	 catch(SQLException | IOException | ClassNotFoundException e) {
-		 logger.info("Failed to get a connection from database.");
-		 e.printStackTrace();
-	 }
- }
- 
-  public Connection getConnectionPool() {
-	  if(!pool.isEmpty()) {
-		  Connection cnx= pool.get(0);
-		  pool.remove(0);
-		  return cnx;
-	  }
-	  return null;
-  }
- 
-  public void setConnectionPool(Connection cnx) {
-	 	  pool.add(cnx);
-  }
+	public void setConnectionPool(Connection cnx) {
+		pool.add(cnx);
+	}
   
-  public void closeAllConnections()  {
-	 try { 
-	  
-		   Iterator<Connection> iteratorPool=pool.iterator();
-		   while(iteratorPool.hasNext()) {
-			   Connection cnx=iteratorPool.next();
-			   iteratorPool.remove();
-			   cnx.close();
-		   }
+	public void closeAllConnections()  {
+		try { 
+			Iterator<Connection> iteratorPool=pool.iterator();
+			while(iteratorPool.hasNext()) {
+				Connection cnx=iteratorPool.next();
+				iteratorPool.remove();
+				cnx.close();
+				}
+		}
+		catch(SQLException e) {
+			logger.info("Error closing connections in JDBCConnectionPool");
+			e.printStackTrace();
+		}	 
+	}
 
-   }
-	catch(SQLException e) {
-		logger.info("Error closing connections in JDBCConnectionPool");
-		e.printStackTrace();
-	}	 
- }
-  
+  public static void main(String[] args) {
+	  System.out.print("+++");
+  }
   
 }
  
