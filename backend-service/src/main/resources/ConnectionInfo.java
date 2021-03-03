4a72c4ebcdd7e1e0c7d8ae4aa96c4a109bf7cbe3
MohamedTloh <tlohmohamed@hotmail.fr>
2021-03-03 15:12:08 +0100

    added constructor and getters

--- backend-service/src/main/resources/ConnectionInfo.java
+++ backend-service/src/main/resources/ConnectionInfo.java
@@ -1,19 +1,40 @@
+package ressources;
+import java.util.Properties;
+
 public class ConnectionInfo {
 	
+	private Properties prop = PropertyLoader.load("app.properties");
 	private String driverClassName;
 	private String url;
 	private String username;
 	private String password;
 	
-	public ConenctionInfo() {
-		
+	public ConenctionInfo() { 
+		driverClassNamer = prop.getProperty(database.driverClassName);
+		url = prop.getProperty(database.url);
+		username = prop.getProperty(database.postgres);
+		password = prop.getProperty(database.password);
 	}
 	
-	/*
-	database.driverClassName= org.postgresql.Driver
-	database.url= jdbc:postgresql://172.31.249.76/test
-	database.username =postgres
-	database.password =root
-	*/
+	public String getDriverClassName() {
+		return driverClassName;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	
+	public String getPassword() {
+		return password;
+	}
 
+	public string toString() {
+		String s = "" + driverClassName + url + username + password;
+		return s
+	}
 }
