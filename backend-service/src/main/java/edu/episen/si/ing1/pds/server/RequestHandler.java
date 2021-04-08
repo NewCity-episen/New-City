a3e5bce796a1106cbd517c9f19bcb9cf508662d6
MohamedTloh <tlohmohamed@hotmail.fr>
2021-04-09 00:58:02 +0200

    request update

--- backend-service/src/main/java/edu/episen/si/ing1/pds/server/RequestHandler.java
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/server/RequestHandler.java
@@ -21,7 +21,9 @@ import shared.code.StudentConfig;
 
 
 public class RequestHandler {
+	
 	private final static Logger logger=LoggerFactory.getLogger(RequestHandler.class.getName());
+	
 	public static Response handle(Request request, Connection cnx) throws JsonMappingException, JsonProcessingException, SQLException {
 		// TODO Auto-generated method stub
 		String requestOrder=request.getRequestOrder();
@@ -30,30 +32,34 @@ public class RequestHandler {
 		String responseBody=null;
 		Statement stmt = cnx.createStatement();
 		final ObjectMapper mapper=new ObjectMapper();
+		
 		if(requestOrder.toUpperCase().equals("SELECT")) {
 			sqlRequest="SELECT * FROM "+ request.getRequestTable();
 			if(studentsList!=null) {
 				sqlRequest+=" WHERE ";
-			for(int i=0;i<studentsList.size();i++) {
-				sqlRequest+="id="+studentsList.get(i).getId();
-				if(!(i==(studentsList.size()-1))) {
-					sqlRequest+=" OR ";
+				for(int i=0;i<studentsList.size();i++) {
+					sqlRequest+="id="+studentsList.get(i).getId();
+					if(!(i==(studentsList.size()-1))) {
+						sqlRequest+=" OR ";
+					}
 				}
-			  }
 			}
 			ResultSet rs= stmt.executeQuery(sqlRequest);
 			Student student;
 			studentsList=new ArrayList<Student>();
+			
 			while(rs.next()) {
-			int id= rs.getInt("id");
-			String name=rs.getString("name");
-			String lastname = rs.getString("lastname");
-			student=new Student(name,lastname,id);
-			studentsList.add(student);
+				int id= rs.getInt("id");
+				String name=rs.getString("name");
+				String lastname = rs.getString("lastname");
+				student=new Student(name,lastname,id);
+				studentsList.add(student);
 			}
+			
 			responseBody="{"+mapper.writeValueAsString(studentsList)+"}";
 			rs.close();
 		}
+
 		else if(requestOrder.toUpperCase().equals("INSERT")) {
 			for(int i=0;i<studentsList.size();i++) {
 				sqlRequest="INSERT INTO "+ request.getRequestTable()+" (name,lastname) VALUES ('"+studentsList.get(i).getName()+"','"+
@@ -63,24 +69,31 @@ public class RequestHandler {
 			}
 		
 		}
+
 		else if(requestOrder.toUpperCase().equals("DELETE")) {
 			
-				for(int i=0;i<studentsList.size();i++) {
+			for(int i=0;i<studentsList.size();i++) {
 			    sqlRequest="DELETE FROM "+ request.getRequestTable();
 			    sqlRequest+=" WHERE "+ "name = '"+  studentsList.get(i).getName()+ "' and lastname = '"+studentsList.get(i).getLastname()+"'";
 			    stmt.executeUpdate(sqlRequest);	
-				responseBody="{\"message\": \"Successfully deleted!\"} "; 
-				
-			
-				}
-				
-			  }	
+				responseBody="{\"message\": \"Successfully deleted!\"} "; 		
+			}	
+		}	
+
 		else if(requestOrder.toUpperCase().equals("UPDATE")) {
 			
+			for(int i=1;i<studentsList.size();i++) {
+			    sqlRequest="UPDATE "+ request.getRequestTable()+
+			    " SET "+ "name = '"+  studentsList.get(i).getName()+ "', lastname = '"+studentsList.get(i).getLastname()+"'"+
+			    " WHERE "+ "id = '"+ i +"'";
+			    stmt.executeUpdate(sqlRequest);	
+			    responseBody="{\"message\": \"Successfully updated!\"} ";
+			}
 		}
-		stmt.close();
-		return new Response(request.getRequestId(),responseBody); 
-	}
+			stmt.close();
+			return new Response(request.getRequestId(),responseBody); 
+		}
+		
 	public static ArrayList<Student> extractValuesFromRequestBody(String requestBody) throws JsonMappingException, JsonProcessingException {
 		final ObjectMapper mapper=new ObjectMapper();
 		StudentConfig students=mapper.readValue(requestBody, StudentConfig.class);
