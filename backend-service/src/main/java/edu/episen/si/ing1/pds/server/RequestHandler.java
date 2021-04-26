fc443fd0eccae675b462aa3b6990299129c6a044
Sarra <sarra.amara@icloud.com>
2021-04-26 12:58:22 +0200

    Modified how the server reads/answers a client request

--- backend-service/src/main/java/edu/episen/si/ing1/pds/server/RequestHandler.java
+++ backend-service/src/main/java/edu/episen/si/ing1/pds/server/RequestHandler.java
@@ -2,9 +2,11 @@ package edu.episen.si.ing1.pds.server;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Map;
 
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
@@ -27,77 +29,63 @@ public class RequestHandler {
 	public static Response handle(Request request, Connection cnx) throws JsonMappingException, JsonProcessingException, SQLException {
 		// TODO Auto-generated method stub
 		String requestOrder=request.getRequestOrder();
-	    ArrayList<Student> studentsList=extractValuesFromRequestBody(request.getRequestBody());
 		String sqlRequest="";
 		String responseBody=null;
+		String valuesJson="[";
 		Statement stmt = cnx.createStatement();
 		final ObjectMapper mapper=new ObjectMapper();
 		
-		if(requestOrder.toUpperCase().equals("SELECT")) {
+		if(requestOrder.toUpperCase().equals("SELECT")) { //this only works for requests that are like this : 
+															//SELECT * FROM TABLE_NAME WHERE column_name='column_value' AND column_name1='column_value1' AND .......;
 			sqlRequest="SELECT * FROM "+ request.getRequestTable();
-			if(studentsList!=null) {
+			if(!request.getRequestBody().equals("{}")) {// If there are values in the RequestBody
+				Map<String,String> valuesMap=mapper.readValue(request.getRequestBody(), Map.class);
 				sqlRequest+=" WHERE ";
-				for(int i=0;i<studentsList.size();i++) {
-					sqlRequest+="id="+studentsList.get(i).getId();
-					if(!(i==(studentsList.size()-1))) {
-						sqlRequest+=" OR ";
-					}
+				for(String key: valuesMap.keySet()) {
+					
+					sqlRequest+=key+"='"+valuesMap.get(key)+"' AND ";
+	
 				}
+				sqlRequest+=";";
+				sqlRequest=sqlRequest.substring(0, sqlRequest.indexOf("AND ;"));
 			}
 			ResultSet rs= stmt.executeQuery(sqlRequest);
-			Student student;
-			studentsList=new ArrayList<Student>();
-			
+			ResultSetMetaData rsM=rs.getMetaData();
 			while(rs.next()) {
-				int id= rs.getInt("id");
-				String name=rs.getString("name");
-				String lastname = rs.getString("lastname");
-				student=new Student(name,lastname,id);
-				studentsList.add(student);
+				valuesJson+="{";
+				for(int i=1;i<=rsM.getColumnCount();i++) {
+					if(!(i==1)) {
+						valuesJson+=",";
+					}
+						Object columnValue=rs.getObject(i);
+						valuesJson+="\""+rsM.getColumnLabel(i)+"\": "+"\""+columnValue+"\"";
+				}
+				if(rs.isLast()) {
+					valuesJson+="}]";
+				}
+				else {
+				valuesJson+="},";
+				}
 			}
 			
-			responseBody="{"+mapper.writeValueAsString(studentsList)+"}";
+			responseBody="{ \"message\": "+valuesJson+"}";
 			rs.close();
 		}
 
 		else if(requestOrder.toUpperCase().equals("INSERT")) {
-			for(int i=0;i<studentsList.size();i++) {
-				sqlRequest="INSERT INTO "+ request.getRequestTable()+" (name,lastname) VALUES ('"+studentsList.get(i).getName()+"','"+
-			    studentsList.get(i).getLastname()+"')";
-				stmt.executeUpdate(sqlRequest);	
-				responseBody="{\"message\": \"Successfully inserted!\"} "; 
-			}
+
 		
 		}
-
 		else if(requestOrder.toUpperCase().equals("DELETE")) {
 			
-			for(int i=0;i<studentsList.size();i++) {
-			    sqlRequest="DELETE FROM "+ request.getRequestTable();
-			    sqlRequest+=" WHERE "+ "name = '"+  studentsList.get(i).getName()+ "' and lastname = '"+studentsList.get(i).getLastname()+"'";
-			    stmt.executeUpdate(sqlRequest);	
-				responseBody="{\"message\": \"Successfully deleted!\"} "; 		
-			}	
 		}	
-
 		else if(requestOrder.toUpperCase().equals("UPDATE")) {
 			
-			for(int i=0;i<studentsList.size();i++) {
-			    sqlRequest="UPDATE "+ request.getRequestTable()+
-			    " SET "+ "name = '"+  studentsList.get(i).getName()+ "', lastname = '"+studentsList.get(i).getLastname()+"'"+
-			    " WHERE "+ "id ="+studentsList.get(i).getId();
-			    stmt.executeUpdate(sqlRequest);	
-			    responseBody="{\"message\": \"Successfully updated!\"} ";
-			}
+
 		}
 			stmt.close();
 			return new Response(request.getRequestId(),responseBody); 
 		}
 		
-	public static ArrayList<Student> extractValuesFromRequestBody(String requestBody) throws JsonMappingException, JsonProcessingException {
-		final ObjectMapper mapper=new ObjectMapper();
-		StudentConfig students=mapper.readValue(requestBody, StudentConfig.class);
-		return students.getStudents();
-	}
 
 }
