fe353e8fe18c7d5f0cb9cac73933647472d8d443
MohamedTloh <tlohmohamed@hotmail.fr>
2021-04-27 17:24:35 +0200

    created class for the location research results

---
+++ client/src/main/java/view/LocationOfferPanel.java
@@ -0,0 +1,16 @@
+package view;
+
+import java.awt.BorderLayout;
+import javax.swing.*;
+
+public class LocationOfferPanel {
+	private static JLabel searchResults=new JLabel("Ici seront les résulats des recherches");
+	private static JPanel LocationOfferPanel=new JPanel();
+	
+	public LocationOfferPanel(){
+		LocationOfferPanel.add(searchResults);
+	}
+	public static JPanel getJPanel() {
+		return LocationOfferPanel;
+	}
+}
